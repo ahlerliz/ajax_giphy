4f13ed146cfd64bda55d8bfda6f0797d6ecf64f7
davisrao <davisrao1@gmail.com>
2021-08-31 17:40:08 -0700

    finished the tool by adding remove button and refactored

--- app.js
+++ app.js
@@ -5,30 +5,57 @@ const APIKEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
 
 console.log("Let's get this party started!");
 
-function getFormValue(){
+
+/**
+ * returns the search term from form
+ */
+function getFormValue() {
     return $("#giphyInput").val();
 }
 
-function showGiphy(responseToObject){
-    $('#giphyBody').append(`<img src="${responseToObject}"></img>`);
-}
-
-async function requestAjaxSearchTerm(searchTerm){
-    let response = await axios.get(GIPHYAPI,{params: {q: searchTerm, api_key: APIKEY}});
+/**
+ * takes search term from user and extracts URL from giphy via AJAX request
+ */
+async function requestAjaxSearchTerm(searchTerm) {
+    let response = await axios.get(GIPHYAPI, { params: { q: searchTerm, api_key: APIKEY } });
     //let responseToObject = JSON.parse(response);
-    let embedUrl = response.data.data[0].images.original.url
+    let embedUrl = response.data.data[0].images.original.url;
     console.log('response: ', response);
     console.log("response data", embedUrl);
-    showGiphy(embedUrl);
+    return embedUrl;
+}
+
+/**
+ * takes the giphy URL and appends it in an image to DOM 
+ */
+
+function showGiphy(embedUrl) {
+    // $('#giphyBody').append(`<img src="${embedUrl}"></img>`);
+    const newImage = $('<img/>').attr("src",embedUrl);
+    $('#giphyBody').append(newImage);
 }
 
 
-$("#searchForm").on("submit", function(e){
+/**
+ * removes all giphys from page
+ */
+
+function emptyGiphyBody() {
+    $("#giphyBody").empty();
+}
+$("#removeButton").on("click", emptyGiphyBody);
+
+
+
+//Break the function on event listener out and call it in line 50
+
+$("#searchForm").on("submit", async function (e) {
     // get the value of the giphy input
-    // e.preventDefault();
-    let searchTerm = getFormValue();
-    requestAjaxSearchTerm(searchTerm);
+    e.preventDefault();
     // take that input and use ajax to search taht string on giphy site
     // that's going to give us a response that we then need to append the response to the page (likely a piece of data within response)
     // pass that to 
+    let searchTerm = getFormValue();
+    let embedUrl = await requestAjaxSearchTerm(searchTerm);
+    showGiphy(embedUrl);
 })
